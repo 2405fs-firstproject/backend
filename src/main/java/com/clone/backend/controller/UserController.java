e786aba2f81c314a50c85ac1a33c7d74688b1f36
jiyoung choi <codingold@naver.com>
2024-07-12 17:51:11 +0900

    4 commit

--- src/main/java/com/clone/backend/controller/UserController.java
+++ src/main/java/com/clone/backend/controller/UserController.java
@@ -3,20 +3,24 @@ import com.clone.backend.model.User;
 import com.clone.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/member")
+@RequestMapping(value = "/user")
 @CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
-    @PostMapping("/post")
+    @PostMapping("/add")
     public String post(@RequestBody User user) {
         userService.saveUser(user);
         return "New member is added";
 
-
+        }
+    @GetMapping ("/getAll")
+    public List<User> getAllUser(){
+        return userService.getAllUser();
 
     }
     }
